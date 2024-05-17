aac4c221a7bfcec07f544e3a76c4a5d66c4ffb2b
Merabi Samkharadze <samkharadzemerab@gmail.com>
2024-05-17 13:55:25 +0400

    Completed ten tasks

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+"use strict";
+
 // დავალება 1
 
 for (i = 5; i < 100; i++) {
@@ -45,3 +47,76 @@ for (item of array4) {
 }
 let average = sum2 / array4.length;
 console.log(average);
+
+// დავალება 6
+
+let array5 = [1, 2, 3, 7, 6, 9];
+
+for (item of array5) {
+  if (item == 7) {
+    continue;
+  }
+  console.log(item);
+}
+
+// დავალება 7
+
+let user = {
+  firstname: "giorgi",
+  lastname: "smith",
+  age: 25,
+  studentstatus: "active",
+};
+
+// console.log(user['studentstatus'])
+console.log(user.studentstatus);
+
+// დავალება 8
+
+let user2 = {
+  name: "anna",
+  age: 20,
+  studentstatus: "active",
+};
+
+// if (user2.age < 18 && user2.studentstatus == "active") {
+//   console.log("hello user");
+// } else if (user2.name == "levani") {
+//   console.log("hello levani");
+// } else if (user2.studentstatus == "active" || user2.age < 25) {
+//   console.log("hello anna");
+// } else {
+//   console.log("error");
+// }
+
+user2.age < 18 && user2.studentstatus == "active"
+  ? console.log("hello user")
+  : user2.name == "levani"
+  ? console.log("hello levani")
+  : user2.studentstatus == "active" || user2.age < 25
+  ? console.log("hello anna")
+  : console.log("error");
+
+// დავალება 9
+
+let array6 = [2, 3, 5, 10, 25, 24, 11, 100, 6, 7, 10];
+
+for (item of array6) {
+  if (item % 2 == 0) {
+    console.log(item);
+  }
+}
+
+// დავალება 10
+
+let users = [
+  { username: "giorgi", status: false },
+  { username: "levani", status: false },
+  { username: "anna", status: true },
+];
+
+for (item of users) {
+  if (item.status == true) {
+    console.log(item);
+  }
+}
